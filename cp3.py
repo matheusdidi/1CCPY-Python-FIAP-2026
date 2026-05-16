d8d8d8c7c0d7577f09f01603747e0e8132e1dc74
matheusdidi <delarasilvamatheus@gmail.com>
2026-05-15 22:03:17 -0300

    Create cp3.py

---
+++ cp3.py
@@ -0,0 +1,31 @@
+temperaturas = [
+    [28, 31, 34, 33],
+    [25, 27, 29, 28],
+    [32, 35, 36, 34],
+    [24, 26, 25, 27]
+]
+
+maior_risco = 0
+sala_maior_risco = 0
+
+for i in range(len(temperaturas)):
+    sala = temperaturas[i]
+
+    media = sum(sala) / len(sala)
+
+    registros_criticos = 0
+
+    for temperatura in sala:
+        if temperatura >= 33:
+            registros_criticos += 1
+
+    print(f"Sala {i + 1}")
+    print(f"Média: {media}")
+    print(f"Registros críticos: {registros_criticos}")
+    print()
+
+    if registros_criticos > maior_risco:
+        maior_risco = registros_criticos
+        sala_maior_risco = i + 1
+
+print(f"Sala com maior risco: Sala {sala_maior_risco}")
