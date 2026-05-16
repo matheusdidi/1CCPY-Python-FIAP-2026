14a754fd8b0da63385d01ff5873e4b5edf2b6b8b
matheusdidi <delarasilvamatheus@gmail.com>
2026-05-15 21:56:27 -0300

    Update cp3.py

--- aula02-variaves-operadores/cp3.py
+++ aula02-variaves-operadores/cp3.py
@@ -10,30 +10,24 @@ temperaturas = [
 maior_risco = 0
 sala_maior_risco = 0
 
-# Percorre cada sala
 for i in range(len(temperaturas)):
     sala = temperaturas[i]
 
-    # Calcula a média
     media = sum(sala) / len(sala)
 
-    # Conta registros críticos (>= 33)
     registros_criticos = 0
 
     for temperatura in sala:
         if temperatura >= 33:
             registros_criticos += 1
 
-    # Exibe os dados da sala
     print(f"Sala {i + 1}")
     print(f"Média: {media}")
     print(f"Registros críticos: {registros_criticos}")
     print()
 
-    # Verifica a sala com maior risco
     if registros_criticos > maior_risco:
         maior_risco = registros_criticos
         sala_maior_risco = i + 1
 
-# Exibe a sala com maior risco
 print(f"Sala com maior risco: Sala {sala_maior_risco}")
